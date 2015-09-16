9cff9052bd8aca475fd6d92a6b172ae4ba48b1af
Sreejith Madhavan <msreejith@gmail.com>
2015-09-16 00:43:27 -0700

    Delete ex1.py

--- PyEx/ex1.py
+++
@@ -1,8 +0,0 @@
-
-print "Hello World!"
-print "Hello Again"
-print "I like typing this."
-print "this is fun."
-print 'Yay! printing.'
-print "I'd much rather you 'not'."
-print 'I "said" do not touch this.'
